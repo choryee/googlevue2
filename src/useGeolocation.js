e4d9bfc31c5d60285b7d46064cdbb3f04817db38
kjh <kjh@emgram.kr>
2023-06-27 18:38:11 +0900

    first commit

---
+++ src/useGeolocation.js
@@ -0,0 +1,22 @@
+/* eslint-disable */ 
+
+import { onUnmounted, onMounted, ref } from "vue";
+
+export function useGeolocation(){
+    const coords=ref({latitude:0, longitude:0});
+    const inSupported='navigator' in window && 'geolocation' in navigator;
+    let watcher=null;
+
+    onMounted(()=>{
+        if(inSupported)
+        watcher=navigator.geolocation.watchPosition(
+            position=>(coords.value = position.coords)
+        )
+    })
+
+    onUnmounted(()=>{
+        if(watcher) navigator.geolocation.clearWatch(watcher)
+    })
+
+    return {coords, inSupported}
+}
